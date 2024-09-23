7ee9045ca614027cd49dcee0f93dda782ba54511
AlexandreDemarquet <alex.demarquet@gmail.com>
2024-09-23 23:25:44 +0200

    modif scrypt build

--- scrypt_build.sh
+++ scrypt_build.sh
@@ -1,5 +1,4 @@
 cd ./worker
 sudo -E /usr/local/go/bin/go build -o /usr/local/bin/worker_test ./cmd/test_worker.go
-cd ..
-cd ./master
+cd .././master
 sudo -E /usr/local/go/bin/go build -o /usr/local/bin/master_test ./cmd/test_master.go
